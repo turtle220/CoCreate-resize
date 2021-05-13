ae260cbbe7a01804d1be0e5df84cd340c0143c83
Konstantinos <talentanakon@gmail.com>
2021-05-14 00:27:11 +0300

    [Update]: optimized function with 8kb

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
 import observer from "@cocreate/observer"
 import "./style.css"
 
-const peak = ['left', 'right', 'top', 'bottom']
-const mouseEvt = "mousemove touchmove"
-const mouseStart = "mousedown touchstart"
-const doc = document.documentElement
-const docStyle = document.defaultView.getComputedStyle(this.resizeWidget)
+let mouseET = "mousemove touchmove"
+let mouseST = "mousedown touchstart"
+let doc = document.documentElement
+let docStyle = document.defaultView.getComputedStyle
+let size
 const coCreateResize = {
     selector: "",
     resizers: [],
     resizeWidgets: [],
     init: function (handleObj) {
-        for (var handleKey in handleObj) if (handleObj.hasOwnProperty(handleKey) && handleKey == "selector") this.selector = handleObj[handleKey]
+        for (let handleKey in handleObj) if (handleObj.hasOwnProperty(handleKey) && handleKey == "selector") this.selector = handleObj[handleKey]
         this.resizers = document.querySelectorAll(this.selector)
-        var _this = this
+        let _this = this
         this.resizers.forEach(function (resize, idx) {
             let resizeWidget = new CoCreateResize(resize, handleObj)
             _this.resizeWidgets[idx] = resizeWidget
         })
     },
     initElement: function (target) {
-        let resizeWidget = new CoCreateResize(target, {dragLeft: "[data-resize_handle='left']", dragRight: "[data-resize_handle='right']",
-                            dragTop: "[data-resize_handle='top']", dragBottom: "[data-resize_handle='bottom']",})
+        let resizeWidget = new CoCreateResize(target, {dragLeft: "[data-resize_handle='left']", dragRight: "[data-resize_handle='right']", dragTop: "[data-resize_handle='top']", dragBottom: "[data-resize_handle='bottom']"})
         this.resizeWidgets[0] = resizeWidget
     },
 }
@@ -33,105 +32,87 @@ function CoCreateResize(resizer, options) {
 CoCreateResize.prototype = {
     init: function (handleObj) {
         if (this.resizeWidget) {
-            this.leftDrag = this.resizeWidget.querySelector(handleObj["dragLeft"])
-            this.rightDrag = this.resizeWidget.querySelector(handleObj["dragRight"])
-            this.topDrag = this.resizeWidget.querySelector(handleObj["dragTop"])
-            this.bottomDrag = this.resizeWidget.querySelector(handleObj["dragBottom"])
+            size = this.resizeWidget
+            this.leftDrag = size.querySelector(handleObj["dragLeft"])
+            this.rightDrag = size.querySelector(handleObj["dragRight"])
+            this.topDrag = size.querySelector(handleObj["dragTop"])
+            this.bottomDrag = size.querySelector(handleObj["dragBottom"])
             this.bindListeners()
             this.initResize()
         }
     },
-    initResize: function () {
-        this.leftDrag && (this.add_remList(this.leftDrag, mouseEvt, e => this.checkDragCorner(e, 'leftTop', this.leftDrag, this.topDrag, e => this.initDrag(e, 'left'), e => this.initDrag(e, 'top')), 'add'), 
-                        this.add_remList(this.leftDrag, mouseEvt, e => this.checkDragCorner(e, 'leftBot', this.leftDrag,this.bottomDrag, e => this.initDrag(e, 'left'), e => this.initDrag(e, 'bottom')), 'add'))
-        this.topDrag && (this.add_remList(this.topDrag, mouseEvt, e => this.checkDragCorner(e, 'topLeft', this.topDrag, this.leftDrag, e => this.initDrag(e, 'top'), e => this.initDrag(e, 'left')), 'add'), 
-                        this.add_remList(this.topDrag, mouseEvt, e => this.checkDragCorner(e, 'topRight', this.topDrag, this.rightDrag, e => this.initDrag(e, 'top'), e => {this.initDrag(e, 'right')}), 'add'))
-        this.rightDrag && (this.add_remList(this.rightDrag, mouseEvt, e => this.checkDragCorner(e, 'rightTop', this.rightDrag, this.topDrag, e => this.initDrag(e, 'right'), e => this.initDrag(e, 'top')), 'add'),
-                        this.add_remList(this.rightDrag, mouseEvt, e => this.checkDragCorner(e, 'rightBot', this.rightDrag, this.bottomDrag, e => this.initDrag(e, 'right'), e => this.initDrag(e, 'bottom')), 'add'))
-        this.bottomDrag && (this.add_remList(this.bottomDrag, mouseEvt, e => this.checkDragCorner(e, 'botLeft', this.bottomDrag, this.leftDrag, e => this.initDrag(e, 'bottom'), e => this.initDrag(e, 'left')), 'add'),
-                        this.add_remList(this.bottomDrag, mouseEvt, e => this.checkDragCorner(e, 'botRight', this.bottomDrag, this.rightDrag, e => this.initDrag(e, 'bottom'), e => this.initDrag(e, 'right')), 'add'))
+    initResize: function() {
+        this.leftDrag && (this.add_remList(this.leftDrag, mouseET, e => this.checkDragCorner(e, 'leftTop', this.leftDrag, this.topDrag, e => this.initDrag(e, 'left'), e => this.initDrag(e, 'top')), 0), 
+                        this.add_remList(this.leftDrag, mouseET, e => this.checkDragCorner(e, 'leftBot', this.leftDrag,this.bottomDrag, e => this.initDrag(e, 'left'), e => this.initDrag(e, 'bot')), 0))
+        this.topDrag && (this.add_remList(this.topDrag, mouseET, e => this.checkDragCorner(e, 'topLeft', this.topDrag, this.leftDrag, e => this.initDrag(e, 'top'), e => this.initDrag(e, 'left')), 0), 
+                        this.add_remList(this.topDrag, mouseET, e => this.checkDragCorner(e, 'topRight', this.topDrag, this.rightDrag, e => this.initDrag(e, 'top'), e => {this.initDrag(e, 'right')}), 0))
+        this.rightDrag && (this.add_remList(this.rightDrag, mouseET, e => this.checkDragCorner(e, 'rightTop', this.rightDrag, this.topDrag, e => this.initDrag(e, 'right'), e => this.initDrag(e, 'top')), 0),
+                        this.add_remList(this.rightDrag, mouseET, e => this.checkDragCorner(e, 'rightBot', this.rightDrag, this.bottomDrag, e => this.initDrag(e, 'right'), e => this.initDrag(e, 'bot')), 0))
+        this.bottomDrag && (this.add_remList(this.bottomDrag, mouseET, e => this.checkDragCorner(e, 'botLeft', this.bottomDrag, this.leftDrag, e => this.initDrag(e, 'bot'), e => this.initDrag(e, 'left')), 0),
+                        this.add_remList(this.bottomDrag, mouseET, e => this.checkDragCorner(e, 'botRight', this.bottomDrag, this.rightDrag, e => this.initDrag(e, 'bot'), e => this.initDrag(e, 'right')), 0))
     },
     initDrag: function (e, type) {
-        let left = type === peak[0]
-        let right = type === peak[1]
-        let top = type === peak[2]
-        let bot = type === peak[3]
+        let left = type === 'left', right = type === 'right', top = type === 'top', bot = type === 'bot'
         this.processIframe()
-        this.startTop = parseInt(docStyle(this.resizeWidget).top, 10)
-        this.startHeight = parseInt(docStyle(this.resizeWidget).height, 10)
-        this.startLeft = parseInt(docStyle(this.resizeWidget).left, 10)
-        this.startWidth = parseInt(docStyle(this.resizeWidget).width, 10)
+        this.startTop = parseInt(docStyle(size).top, 10)
+        this.startHeight = parseInt(docStyle(size).height, 10)
+        this.startLeft = parseInt(docStyle(size).left, 10)
+        this.startWidth = parseInt(docStyle(size).width, 10)
         this.startY = e.touches && (top || bot) ? e.touches[0].clientY : e.clientY
         this.startX = e.touches && (left || right) ? e.touches[0].clientX : e.clientX
-        top && this.add_remList(doc, mouseEvt, this.doTopDrag, 'add')
-        bot && this.add_remList(doc, mouseEvt, this.doBottomDrag, 'add')
-        right && this.add_remList(doc, mouseEvt, this.doRightDrag, 'add')
-        left && this.add_remList(doc, mouseEvt, this.doLeftDrag, 'add')
-        this.add_remList(doc, "mouseup touchend", this.stopDrag, 'add')
+        top && this.add_remList(doc, mouseET, this.doTopDrag, 0)
+        bot && this.add_remList(doc, mouseET, this.doBotDrag, 0)
+        right && this.add_remList(doc, mouseET, this.doRightDrag, 0)
+        left && this.add_remList(doc, mouseET, this.doLeftDrag, 0)
+        this.add_remList(doc, "mouseup touchend", this.stopDrag, 0)
     },
     doTopDrag: function (e) {
-        if(e.touches) e = e.touches[0]
-        let top = this.startTop + e.clientY - this.startY
-        let height = this.startHeight - e.clientY + this.startY
+        e.touches && (e = e.touches[0])
+        let top = this.startTop + e.clientY - this.startY, height = this.startHeight - e.clientY + this.startY
         if (top < 10 || height < 10) return
-        this.resizeWidget.style.top = top + "px"
-        this.resizeWidget.style.height = height + "px"
+        size.style.top = top + "px"
+        size.style.height = height + "px"
     },
-    doBottomDrag: function (e) {
-        let height = 0
-        height = e.touches ? (this.startHeight + e.touches[0].clientY - this.startY) : this.startHeight + e.clientY - this.startY
+    doBotDrag: function (e) {
+        let height = e.touches ? this.startHeight + e.touches[0].clientY - this.startY : this.startHeight + e.clientY - this.startY
         if (height < 10) return
-        this.resizeWidget.style.height = height + "px"
+        size.style.height = height + "px"
     },
     doLeftDrag: function (e) {
         if(e.touches) e = e.touches[0]
-        let left = this.startLeft + e.clientX - this.startX
-        let width = this.startWidth - e.clientX + this.startX
+        let left = this.startLeft + e.clientX - this.startX, width = this.startWidth - e.clientX + this.startX
         if (width < 10) return
-        this.resizeWidget.style.left = left + "px"
-        this.resizeWidget.style.width = width + "px"
+        size.style.left = left + "px"
+        size.style.width = width + "px"
     },
     doRightDrag: function (e) {
-        let width = 0
-        width = e.touches ? (this.startWidth + e.touches[0].clientX - this.startX) : (this.startWidth + e.clientX - this.startX)
+        let width = e.touches ? this.startWidth + e.touches[0].clientX - this.startX : this.startWidth + e.clientX - this.startX
         if (width < 10) return
-        this.resizeWidget.style.width = width + "px"
+        size.style.width = width + "px"
     },
     stopDrag: function () {
-        this.resizeWidget.querySelectorAll("iframe").forEach(function (item) {
-            item.style.pointerEvents = null
-        })
-        this.add_remList(doc, mouseEvt, this.doTopDrag, 'remove')
-        this.add_remList(doc, mouseEvt, this.doBottomDrag, 'remove')
-        this.add_remList(doc, mouseEvt, this.doLeftDrag, 'remove')
-        this.add_remList(doc, mouseEvt, this.doRightDrag, 'remove')
-        this.add_remList(doc, mouseEvt, this.stopDrag, 'remove')
+        size.querySelectorAll("iframe").forEach(function (item) { item.style.pointerEvents = null })
+        this.add_remList(doc, mouseET, this.doTopDrag, 1)
+        this.add_remList(doc, mouseET, this.doBotDrag, 1)
+        this.add_remList(doc, mouseET, this.doLeftDrag, 1)
+        this.add_remList(doc, mouseET, this.doRightDrag, 1)
+        this.add_remList(doc, mouseET, this.stopDrag, 1)
+        this.add_remList(doc, "mouseup touchend", this.stopDrag, 0)
     },
-    checkDragCorner: function (e, type, callFunc1, callFunc2, initDrag1, initDrag2) {
-        let topLeft = type === 'topLeft'
-        let leftTop = type === 'leftTop'
-        let rightTop = type === 'rightTop'
-        let topRight = type === 'topRight'
-        let botLeft = type === 'botLeft'
-        let leftBot = type === 'leftBot'
-        let botRight = type === 'botRight'
-        let rightBot = type === 'rightBot'
+    checkDragCorner: function (e, type, call, com, initDrag1, initDrag2) {
+        let topLeft = type === 'topLeft', leftTop = type === 'leftTop', rightTop = type === 'rightTop', topRight = type === 'topRight', botLeft = type === 'botLeft', leftBot = type === 'leftBot', botRight = type === 'botRight', rightBot = type === 'rightBot'
+        let offsetPos = topLeft ? e.clientX - this.getTLDistance(this.topDrag, 1) + doc.scrollLeft : leftTop ? e.clientY - this.getTLDistance(this.leftDrag, 0) + doc.scrollTop : (topRight || botRight) ? this.getTLDistance(this.rightDrag, 1) - e.clientX - doc.scrollLeft : rightTop ? e.clientY - this.getTLDistance(this.topDrag, 0) + doc.scrollTop :
+                        botLeft ? e.clientX - this.getTLDistance(this.bottomDrag, 1) + doc.scrollLeft : (leftBot || rightBot) ? this.getTLDistance(this.bottomDrag, 0) - e.clientY - doc.scrollTop : null
         if (e.touches) e = e.touches[0]
-        let offsetPos = topLeft ? e.clientX - this.getT_LDistance(this.topDrag, 1) + doc.scrollLeft :
-                    leftTop ? e.clientY - this.getT_LDistance(this.leftDrag, 0) + doc.scrollTop :
-                    topRight || botRight ? this.getT_LDistance(this.rightDrag, 1) - e.clientX - doc.scrollLeft :
-                    rightTop ? e.clientY - this.getT_LDistance(this.topDrag, 0) + doc.scrollTop :
-                    botLeft ? e.clientX - this.getT_LDistance(this.bottomDrag, 1) + doc.scrollLeft :
-                    leftBot || rightBot? this.getT_LDistance(this.bottomDrag, 0) - e.clientY - doc.scrollTop : null
-        this.add_remList(callFunc1, mouseStart, initDrag1, 'remove')
-        this.add_remList(callFunc1, mouseStart, initDrag2, 'remove')
-        this.add_remList(callFunc1, mouseStart, initDrag1, 'add')
-        if (offsetPos < this.cornerSize && callFunc2) {
+        this.add_remList(call, mouseST, initDrag1, 1)
+        this.add_remList(call, mouseST, initDrag2, 1)
+        this.add_remList(call, mouseST, initDrag1, 0)
+        if (offsetPos < this.cornerSize && com) {
             this.topDrag.style.cursor = topLeft ? "se-resize" : topRight ? "ne-resize" : "s-resize"
             this.leftDrag.style.cursor = leftTop ? "se-resize" : leftBot ? "ne-resize" : "e-resize"
             this.rightDrag.style.cursor = rightTop ? "ne-resize" : rightBot ? "se-resize" : "e-resize"
             this.bottomDrag.style.cursor = botLeft ? "ne-resize" : botRight ? "se-resize" : "s-resize"
-            this.add_remList(callFunc1, mouseStart, initDrag2, 'add')
+            this.add_remList(call, mouseST, initDrag2, 0)
         }
     },
     bindListeners: function () {
@@ -139,31 +120,29 @@ CoCreateResize.prototype = {
         this.doLeftDrag = this.doLeftDrag.bind(this)
         this.doTopDrag = this.doTopDrag.bind(this)
         this.doRightDrag = this.doRightDrag.bind(this)
-        this.doBottomDrag = this.doBottomDrag.bind(this)
+        this.doBotDrag = this.doBotDrag.bind(this)
         this.stopDrag = this.stopDrag.bind(this)
         this.checkDragCorner = this.checkDragCorner.bind(this)
     },
-    getT_LDistance: function (elem, type) {
-        var location = 0
-        if (elem.offsetParent) {
+    getTLDistance: function (ele, type) {
+        let location = 0
+        if (ele.offsetParent) {
             do {
-                location += type === 0 ? elem.offsetTop : elem.offsetLeft
-                elem = elem.offsetParent
-            } while (elem)
+                location += type === 0 ? ele.offsetTop : ele.offsetLeft
+                ele = ele.offsetParent
+            } while (ele)
         }
         return location >= 0 ? location : 0
     },
-    add_remList: function (element, eventNames, listener, type) {
-        var events = eventNames.split(" ")
-        for (var i = 0, iLen = events.length; i < iLen; i++) {
-            type === 'add' && element.addEventListener(events[i], listener, false)
-            type === 'remove' && element.removeEventListener(events[i], listener, false)
+    add_remList: function (ele, evtNames, listener, type) {
+        let evts = evtNames.split(" ")
+        for (let i = 0, iLen = evts.length; i < iLen; i++) {
+            type === 0 && ele.addEventListener(evts[i], listener, false)
+            type === 1 && ele.removeEventListener(evts[i], listener, false)
         }
     },
     processIframe: function () {
-        this.resizeWidget.querySelectorAll("iframe").forEach(function (item) {
-            item.style.pointerEvents = "none"
-        })
+        size.querySelectorAll("iframe").forEach(function (item) { item.style.pointerEvents = "none" })
     },
 }
 observer.init({
